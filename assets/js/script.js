597ae3a282845797a7258d3d58f9a4118130eac1
daianaadepaula <daianaadepaula1@gmail.com>
2022-08-14 07:44:04 -0300

    img, js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const tenis1 = document.getElementById('tenis-1');
+const tenis2 = document.getElementById('tenis-2');
+
+const arrowRight = document.getElementById('right');
+const arrowLeft = document.getElementById('left');
+
+arrowLeft.addEventListener('click', () => {
+    tenis1.style.left = '650px';
+    tenis2.style.left = '-50px';
+});
+
+arrowRight.addEventListener('click', () => {
+    tenis1.style.left = '-50px';
+    tenis2.style.left = '-650px';
+});
